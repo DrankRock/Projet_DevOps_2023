10648f139af2cbe9d2dfa3fe023bb1a3b47be8f3
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-04-19 11:24:52 +0200

    updated project to have a 100% coverage

--- ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/App.java
+++ ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/App.java
@@ -6,6 +6,9 @@ package fr.uga.erods.projectDevOps;
  */
 public class App 
 {
+	public void printNiceStuff() {
+		System.out.println("Printing nice stuff in App");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World! This is ProjectDevOps !" );
