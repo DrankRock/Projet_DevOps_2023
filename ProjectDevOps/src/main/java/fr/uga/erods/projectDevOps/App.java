18895ca37c83add19e760e555d4cdb55822e0fc0
Evan1700 <evan17.pereira@gmail.com>
2023-04-24 14:18:08 +0200

    feat/fix: remaniement de la structure et entiereté des fonctions implémentées.

--- ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/App.java
+++ ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/App.java
@@ -1,32 +1,48 @@
 package fr.uga.erods.projectDevOps;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileNotFoundException;
+
 public class App 
 {
-    public static void main( String[] args ) throws Exception
-    {
-        DataFrame test = new DataFrame("fichierCSV.csv");
-        test.printDataframe();
-        System.out.println("");
-        
-        int[] index = {0,2,5};
-        String[][] dL = test.selectLineDataframe(index);
-        for(int i = 0; i < dL.length;i++) {
-			for(int j = 0; j < dL[i].length;j++) {	
-				System.out.print(dL[i][j]+" ");
-				
-			}
-			System.out.println("");
-		}
-        System.out.println("");
-        
-        String[] dC = test.selectColumnDataframe("METIER");
-        for(int i = 0; i < dC.length;i++) {	
-			System.out.println(dC[i]);	
-		}
-
-    }
+	public static void main(String[] args) {
+	    try {
+	        DataFrame df = new DataFrame("fichierCSV.csv");
+	        df.display();
+	        System.out.println();
+	        
+	        df.displayFirstNRows(5);
+	        System.out.println();
+	        
+	        df.displayLastNRows(4);
+	        System.out.println();
+	        
+	        int[] rowIndices = {0, 2, 4};
+	        DataFrame dfb = df.selectRows(rowIndices);
+	        dfb.display();
+	        System.out.println();
+	        
+	        String[] columnLabels = {"METIER", "GENRE"};
+	        DataFrame newDf = df.selectColumns(columnLabels);
+	        newDf.display();
+	        System.out.println();
+	        
+	        String[] rowLabels = {"2", "7"};
+	        DataFrame locDf = df.loc(rowLabels, columnLabels);
+	        locDf.display();
+	        System.out.println();
+	        
+	        
+	        System.out.println(df.mean("AGE"));
+	        System.out.println();
+	        
+	        System.out.println(dfb.min("AGE"));
+	        System.out.println();
+	        
+	        System.out.println(df.max("AGE"));
+	        System.out.println();
+	        
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    }
+	}
 }
--- ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/DataFrame.java
+++ ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/DataFrame.java
@@ -1,89 +1,197 @@
 package fr.uga.erods.projectDevOps;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class DataFrame {
-	
-	InitializeDataFrame dataFrameInit;
-	String[][] dataFrame;
-	public DataFrame(String csvFile) {
-		dataFrameInit = new InitializeDataFrame();
-		dataFrame = dataFrameInit.DataFrameTab(csvFile);
-	}
-	
-	public DataFrame(String[] array) {
-		
-	}
-	
-	public DataFrame(String[][] array) {
-		
-	}
-	
-	public void printFirstLine(int numberOfLine) throws Exception {
-		
-		if(numberOfLine < this.dataFrame.length) {
-			for(int i = 0; i < numberOfLine+1;i++) {
-				for(int j = 0; j < this.dataFrame[i].length;j++) {	
-					System.out.print(this.dataFrame[i][j]+" ");
-					
-				}
-				System.out.println("");
-			}
-		} else {
-			throw new Exception("Out of bound exception"); 
-		}
-	}
-	
-	public void printLastLine(int numberOfLine) throws Exception {
-		for(int j = 0; j < this.dataFrame[0].length;j++) {	
-			System.out.print(this.dataFrame[0][j]+" ");
-		}
-		System.out.println("");
-		if(numberOfLine < this.dataFrame.length) {
-			for(int i = this.dataFrame.length - numberOfLine; i < this.dataFrame.length;i++) {
-				for(int j = 0; j < this.dataFrame[i].length;j++) {	
-					System.out.print(this.dataFrame[i][j]+" ");
-					
-				}
-				System.out.println("");
-			}
-		} else {
-			throw new Exception("Out of bound exception"); 
-		}
-	}
-	
-	
-	public void printDataframe()
-    {
-		for(int i = 0; i < this.dataFrame.length;i++) {
-			for(int j = 0; j < this.dataFrame[i].length;j++) {	
-				System.out.print(this.dataFrame[i][j]+" ");
-				
-			}
-			System.out.println("");
-		}
-		
-	
+    private List<String[]> data;
+    private String[] headers;
+
+    public DataFrame(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filename));
+        data = new ArrayList<>();
+
+        if (scanner.hasNextLine()) {
+            headers = scanner.nextLine().split(",");
+        }
+
+        while (scanner.hasNextLine()) {
+            String[] row = scanner.nextLine().split(",");
+            data.add(row);
+        }
+        scanner.close();
+    }
+    
+    public DataFrame(String[] headers, List<String[]> data) {
+        this.headers = headers;
+        this.data = data;
     }
 
-	
-	public String[][] selectLineDataframe(int [] line){
-		
-		String[][] d = new String[line.length][this.dataFrame[0].length];
-		for(int i=0;i<line.length;i++) {
-			d[i] = this.dataFrame[line[i]];
-		}
-		return d;	
-	}
-	
-public String[] selectColumnDataframe(String label){
-	
-		int labelIndex = 0;
-		for(int i = 0 ; i<this.dataFrame[0].length; i++) {
-			if(this.dataFrame[0][i].equals(label)) labelIndex = i;
-		}
-		String[] d = new String[this.dataFrame.length];
-		for(int i=0;i<this.dataFrame.length;i++) {
-			d[i] = this.dataFrame[i][labelIndex];
-		}
-		return d;	
-	}
+    public void display() {
+
+        for (String header : headers) {
+            System.out.print(header + "\t");
+        }
+        System.out.println();
+
+        for (String[] row : data) {
+            for (String cell : row) {
+                System.out.print(cell + "\t");
+            }
+            System.out.println();
+        }
+    }
+    
+    public void displayFirstNRows(int n) {
+
+        for (String header : headers) {
+            System.out.print(header + "\t");
+        }
+        System.out.println();
+
+        for (int i = 0; i < n && i < data.size(); i++) {
+            String[] row = data.get(i);
+            for (String cell : row) {
+                System.out.print(cell + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void displayLastNRows(int n) {
+    	
+        for (String header : headers) {
+            System.out.print(header + "\t");
+        }
+        System.out.println();
+
+        int start = Math.max(0, data.size() - n);
+        for (int i = start; i < data.size(); i++) {
+            String[] row = data.get(i);
+            for (String cell : row) {
+                System.out.print(cell + "\t");
+            }
+            System.out.println();
+        }
+    }
+    
+    public DataFrame selectRows(int[] rowIndices) {
+        String[] newHeaders = Arrays.copyOf(this.headers, this.headers.length);
+
+        List<String[]> newData = new ArrayList<>();
+        for (int rowIndex : rowIndices) {
+            if (rowIndex >= 0 && rowIndex < this.data.size()) {
+                newData.add(Arrays.copyOf(this.data.get(rowIndex), this.headers.length));
+            }
+        }
+
+        return new DataFrame(newHeaders, newData);
+    }
+    
+    private int getColumnIndex(String columnLabel) {
+        for (int i = 0; i < this.headers.length; i++) {
+            if (this.headers[i].equals(columnLabel)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public DataFrame selectColumns(String[] columnLabels) {
+        String[] newHeaders = Arrays.copyOf(columnLabels, columnLabels.length);
+
+        List<String[]> newData = new ArrayList<>();
+        for (String[] row : this.data) {
+            String[] newRow = new String[columnLabels.length];
+            int columnIndex = 0;
+            for (String header : columnLabels) {
+                int index = getColumnIndex(header);
+                if (index != -1) {
+                    newRow[columnIndex] = row[index];
+                }
+                columnIndex++;
+            }
+            newData.add(newRow);
+        }
+
+        return new DataFrame(newHeaders, newData);
+    }
+    
+    public DataFrame loc(String[] rowLabels, String[] columnLabels) {
+        int[] rowIndexes = new int[rowLabels.length];
+        int[] columnIndexes = new int[columnLabels.length];
+        for (int i = 0; i < rowLabels.length; i++) {
+            rowIndexes[i] = getRowIndex(rowLabels[i]);
+        }
+        for (int i = 0; i < columnLabels.length; i++) {
+            columnIndexes[i] = getColumnIndex(columnLabels[i]);
+        }
+        String[] newHeaders = Arrays.copyOf(columnLabels, columnLabels.length);
+        List<String[]> newData = new ArrayList<>();
+        for (int rowIndex : rowIndexes) {
+            String[] row = this.data.get(rowIndex);
+            String[] newRow = new String[columnLabels.length];
+            for (int i = 0; i < columnIndexes.length; i++) {
+                newRow[i] = row[columnIndexes[i]];
+            }
+            newData.add(newRow);
+        }
+        return new DataFrame(newHeaders, newData);
+    }
+
+    private int getRowIndex(String rowLabel) {
+        for (int i = 0; i < this.data.size(); i++) {
+            String[] row = this.data.get(i);
+            if (row[0].equals(rowLabel)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public double mean(String columnName) {
+        int columnIndex = getColumnIndex(columnName);
+        double sum = 0.0;
+        int count = 0;
+        for (String[] row : this.data) {
+            try {
+                double value = Double.parseDouble(row[columnIndex]);
+                sum += value;
+                count++;
+            } catch (NumberFormatException e) {
+            	
+            }
+        }
+        if (count == 0) {
+            throw new IllegalArgumentException("Pas de valeurs numériques dans la colonne " + columnName);
+        }
+        return sum / count;
+    }
+
+    public String max(String columnName) {
+        int columnIndex = getColumnIndex(columnName);
+        String maxValue = null;
+        for (String[] row : this.data) {
+            if (maxValue == null || row[columnIndex].compareTo(maxValue) > 0) {
+                maxValue = row[columnIndex];
+            }
+        }
+        return maxValue;
+    }
+
+    public String min(String columnName) {
+        int columnIndex = getColumnIndex(columnName);
+        String minValue = null;
+        for (String[] row : this.data) {
+            if (minValue == null || row[columnIndex].compareTo(minValue) < 0) {
+                minValue = row[columnIndex];
+            }
+        }
+        return minValue;
+    }
+
 }
