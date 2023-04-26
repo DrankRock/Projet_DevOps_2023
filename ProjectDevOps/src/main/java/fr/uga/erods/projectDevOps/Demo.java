7dbbddc29596a18787ae8f3209eacb152ee6900b
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-04-26 09:59:34 +0200

    Added full demo

--- ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/Demo.java
+++ ProjectDevOps/src/main/java/fr/uga/erods/projectDevOps/Demo.java
@@ -10,34 +10,48 @@ public class Demo {
 		// TODO Auto-generated method stub
 		Demo d = new Demo();
 		String filePath = "src/main/java/fr/uga/erods/projectDevOps/demo.csv";
-		String mode = "[initialize";
+		d.setMode("initialize");
 		d.p("Create Original DataFrame");
 		try {
 			
 			DataFrame df = new DataFrame(filePath);
-			d.dPrint("[initialize] - DataFrame created", 1);
-			d.dPrint("[Basic Operation] - Display DataFrame", 0);
-			d.dPrint(df.toString(), 0);
-			d.dPrint(, 0);
+			d.p("DataFrame created", 1);
+			d.setMode("Basic Operations");
+			d.p("Display DataFrame", df.toString());
+			
+			d.p("Head of 3 rows\n", df.head(3).toString());
+			d.p("Tail of 3 rows\n", df.tail(3).toString());
+			
+			d.p("Select rows 1, 3, 4\n", df.selectRows(new int[] {1, 3, 4}).toString());
+			d.p("Select columns NAME and USERNAME",df.selectColumns(new String[] {"NAME", "USERNAME"}).toString());
+			
+			d.setMode("Advanced Operations");
+			
+			d.p("Select columns NAME and USERNAME, rows 2, 5, 7\n", df.loc(new String[] {"NAME", "USERNAME"}, new int[] {2, 5, 7}).toString());
+			
+			d.p("Reminder : this is the Age column", df.selectColumns("AGE").toString());
+			d.p("Calculate the mean of the age column", Double.toString(df.mean("AGE")));
+			d.p("Calculate the min of the age column", Double.toString(df.min("AGE")));
+			d.p("Calculate the max of the age column", Double.toString(df.mean("AGE")));
 			
 			
 			
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			d.dPrint("[initialize] - Error : File not found ", 3);
+			d.setMode("Error");
+			d.p("File not found exception", 3);
 			System.exit(1);
-		}
-		
-		
-		
+		}	
 	}
 	
-	public void p(String s) {
-		System.out.println(s);
-	}
 	
-	public void dPrint(String toPrint, int err) {
+	/**
+	 * Colored print, with success, warning and error modes
+	 * @param toPrint the string to print
+	 * @param err the color, 1 for green, 2 for yellow, 3 for red, anything else in white
+	 */
+	public void p(String toPrint, int err) {
 		String code = "";
 	    if (err == 3) {
 	        code = "\u001B[31m"; // red
@@ -46,12 +60,44 @@ public class Demo {
 	    } else if (err == 1) {
 	        code = "\033[1;32m"; // green
 	    }
-	    System.out.println(code + mode + toPrint + "\u001B[0m");
-			
+	    System.out.println(code + mode + toPrint + "\u001B[0m");	
 	}
 	
+	/**
+	 * Colored print, with success, warning and error modes
+	 * @param toPrint the string to print
+	 * @param err the color, 1 for green, 2 for yellow, 3 for red, anything else in white
+	 */
+	public void p(String title, String toPrint, int err) {
+		String code = "";
+	    if (err == 3) {
+	        code = "\u001B[31m"; // red
+	    } else if (err == 2) {  
+	        code = "\u001B[33m"; // yellow
+	    } else if (err == 1) {
+	        code = "\033[1;32m"; // green
+	    }
+	    System.out.println(code + mode + title + "\u001B[0m" + "\n" + toPrint);	
+	}
+	
+	public void p(String s, String s2) {
+		this.p(s, s2, 2);
+	}
+	
+	/**
+	 * Print a regular message
+	 * @param s message to print
+	 */
+	public void p(String s) {
+		this.p(s, 2);
+	}
+	
+	/**
+	 * Set the mode of the Demo Printer, which will be displayed as "[mode] - ..." before each text
+	 * @param s the Mode
+	 */
 	public void setMode(String s) {
-		this.mode = "["+s+"]";
+		this.mode = "["+s+"] - ";
 	}
 
 }
--- docker_args.sh
+++ docker_args.sh
@@ -4,18 +4,4 @@
 
 jarFile="/app/target/project-devops-0.0.1.jar"
 
-# Check the argument
-if [ "$1" == "test" ]; then
-  echo "Running tests .. "
-  java -jar ${jarFile}
-elif [ "$1" == "run" ]; then
-  # Shift is used to shift the arguments to the left, so $1 becomes $0, and 
-  # here the $1 run is not necessary anymore, so $1 becomes the first command
-  # line argument after the run
-  shift
-  echo "Running the application with arguments: $@"
-  java -jar ${jarFile} "$@" # And there, $@ will show all the arguments after the run
-else
-  echo "Invalid argument. Please provide 'test' or 'run'."
-  exit 1
-fi
+java -jar ${jarFile}
